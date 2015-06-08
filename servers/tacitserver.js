78feab7d1a466dab7f86761274146a474241bce2
Willem van Heemstra <wvanheemstra@icloud.com>
2015-06-08 18:24:58 +0100

    Updated

--- servers/tacitserver.js
+++ servers/tacitserver.js
@@ -1,11 +1,33 @@
-//var tacitServer = require('tacit-server');
-
-
+/*
+* MASK server
+*
+*/
+/**
+* Module dependencies.
+*/
+var tacitServer = require('tacit-server');
 /*
 * CONFIGS - The Configurations
 */
 config = require('../configs/server.js');
 var configs = config.configs,
 server_prefix = configs.server_prefix || 'MASK';
-
-console.log(server_prefix + " - Node Version: " + process.version);
+tacitServer.setConfigs(configs);
+/*
+* ROUTER - The Router
+*/
+var router = require('../routers/router.js');
+tacitServer.setRouter(router);
+/*
+* ROUTES - The Routes
+*/
+var routes = tacitServer.routes = require('../routes'); // it seems that we have to start each required file as its own var
+/*
+* SERVICES - The Services
+*/
+var services = tacitServer.services = require('../routes/services'); // it seems that we have to start each required file as its own var
+var testService = tacitServer.testService = require('../services/test');
+/*
+* LISTEN
+*/
+tacitServer.listen();
